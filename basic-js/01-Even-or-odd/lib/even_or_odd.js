28a65929bb9e16e37d30148572a4fce9841e297c
alex wilson <alex@innerchapter.co>
2020-07-07 21:44:23 +0800

    even or odd

---
+++ basic-js/01-Even-or-odd/lib/even_or_odd.js
@@ -0,0 +1,18 @@
+evenOrOdd = (number) => {
+  //control flow
+    // TODO: this should return "even" if the number is even, "odd" otherwise
+
+  //control flow
+    // TODO: this should return "even" if the number is even, "odd" otherwise
+
+if (number % 2 ==0) {
+  return("even");
+} else {
+  return("odd");
+  }
+}
+
+evenOrOdd(4)
+evenOrOdd(2)
+evenOrOdd(3)
+
